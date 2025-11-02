a6b94e758d1167a68379c5af2fe31211a7fefb0d
Mateus dos Santos Barbosa <95194717+MateusdSB@users.noreply.github.com>
2025-11-02 02:19:22 -0300

    Feature/tela de menu (#4)
    * feat: criação classe e tabela favorito
    
    * chore: ignorar banco local
    
    * chore: ignorar banco local
    
    * feat: implementação da tela favoritos (#2)
    
    * feat: classe MainApp e classe OlhoVivoClient
    
    * feat: criação da tela Main e inserção do update classe favorito

---
+++ src/main/java/api/OlhoVivoClient.java
@@ -0,0 +1,108 @@
+package api;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+public class OlhoVivoClient {
+    private static final String BASE = "https://api.olhovivo.sptrans.com.br/v2.1";
+
+    private  final HttpClient http = HttpClient.newBuilder()
+            .cookieHandler(new CookieManager(null, CookiePolicy.ACCEPT_ALL))
+            .build();
+
+    private  final ObjectMapper M = new ObjectMapper();
+    private boolean autenticado = false;
+
+
+    public boolean isAutenticado(){
+        return autenticado;
+    }
+
+    //método para realizar a autenticação na API
+    public boolean autenticar(String token) throws Exception {
+        String url = BASE + "/Login/Autenticar?token=" +
+                URLEncoder.encode(token.trim(), StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .version(HttpClient.Version.HTTP_1_1)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString("{}"))
+                .build();
+
+        HttpResponse<String> response = http.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body() != null ? response.body().trim() : "";
+
+
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("HTTP " + response.statusCode() + ": " + body);
+        }
+        autenticado = "true".equalsIgnoreCase(body);
+        return autenticado;
+    }
+
+    /*
+    public boolean autenticar(String token)throws Exception{
+        String url = BASE + "/Login/Autenticar?token=" +
+                URLEncoder.encode(token, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .POST(HttpRequest.BodyPublishers.ofString(""))
+                .header("Accept","application/json")
+                .header("User-Agent","JavaHttpClient/NaRota")
+                .build();
+
+        HttpResponse<String> response = http.send(request,HttpResponse.BodyHandlers.ofString());
+        String body = response.body() != null ? response.body().trim() : "";
+
+        System.out.println("=== DEBUG AUTENTICAR ===");
+        System.out.println("URL: " + url);
+        System.out.println("Status: " + response.statusCode());
+        System.out.println("Body: " + body);
+        System.out.println("========================");
+
+        if(response.statusCode()!= 200){
+            throw new RuntimeException("HTTP " + response.statusCode()+ ": " + response.body());
+        }
+
+        autenticado = "true".equalsIgnoreCase(body);
+        return autenticado;
+
+    }*/
+
+    //retorna o JSON com o resultado da busca
+    public JsonNode buscarLinha(String termosBusca)throws Exception{
+        if(!autenticado){
+            throw new IllegalStateException("Precisa se autenticar primeiro");
+        }
+
+        String url = BASE +"/Linha/Buscar?termosBusca=" +
+                URLEncoder.encode(termosBusca, StandardCharsets.UTF_8);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .header("Accept","application/json")
+                .build();
+
+        HttpResponse<String> response = http.send(request,HttpResponse.BodyHandlers.ofString());
+        if(response.statusCode()!= 200){
+            throw new RuntimeException("HTTP " + response.statusCode()+": " + response.body());
+        }
+
+        return M.readTree(response.body());
+    }
+
+}
